8adab4470cd1089bf2c062a559ec9c4b15899621
trobanga <d@niel.re>
2016-05-11 21:39:12 +0200

    even better logging

--- src/caldavserver.py
+++ src/caldavserver.py
@@ -14,11 +14,11 @@ class CalDAVserver():
         self.url = url
         self.url_nick = nick
         self.client = caldav.DAVClient(url)
-        logging.debug('client: {}'.format(self.client))
+        logging.debug('CalDAVserver.__init__.client: {}'.format(self.client))
         self.principal = self.client.principal()
-        logging.debug('principal: {}'.format(self.principal))
+        logging.debug('CalDAVserver.__init__.principal: {}'.format(self.principal))
         self.calendars = self.principal.calendars()
-        logging.debug('calendars: {}'.format(self.calendars))
+        logging.debug('CalDAVserver.__init__.calendars: {}'.format(self.calendars))
 
         # get calendar IDs
         props = [c.get_properties([caldav.elements.dav.Href(),
@@ -36,14 +36,14 @@ class CalDAVserver():
         """
         cal_id = sanity_check.trailing_slash(cal_id)
         if any([i.endswith(cal_id) for i in self.calendar_IDs]):
-            raise Exception("ID {} already exists in url {}.".format(cal_id, self.url))
+            raise Exception("CalDAVserver.create_calendar: ID {} already exists in url {}.".format(cal_id, self.url))
         if name in self.calendar_DisplayNames:
-            raise Exception("Calendar name {} already exists in url {}.".format(name, self.url))
+            raise Exception("CalDAVserver.create_calendar: Calendar name {} already exists in url {}.".format(name, self.url))
         try:
             self.principal.make_calendar(name=name, cal_id=cal_id)
             logging.info("Created new calendar {}, {}".format(name, cal_id))
             self.calendars = self.principal.calendars()
-            logging.debug('calendars: {}'.format(self.calendars))
+            logging.debug('CalDAVserver.create_calendar: calendars: {}'.format(self.calendars))
             self.calendar_IDs.append(cal_id)
             logging.info("Added new calendar with ID {} to url {}.".format(cal_id, self.url))
         except Exception as e:
@@ -62,6 +62,6 @@ class CalDAVserver():
                     c.delete()
                     logging.info("Deleted calendar with ID {} from url {}".format(cal_id, self.url))
                     return
-            raise Exception("Calendar with ID {} does not exist in url.".format(cal_id, self.url))
+            raise Exception("CalDAVserver.delete_calendar: Calendar with ID {} does not exist in url.".format(cal_id, self.url))
         except Exception as e:
             logging.debug(e)
