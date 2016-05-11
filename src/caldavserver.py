b053a79bd8ced75b529d29007d94803e6f381e66
trobanga <d@niel.re>
2016-05-11 21:25:49 +0200

    added delete function; check that cal_id and name do not already exist when creating new calendars

--- src/caldavserver.py
+++ src/caldavserver.py
@@ -3,13 +3,15 @@
 import sanity_check
 import caldav
 import logging
+import config
 
 
 class CalDAVserver():
     """
-    Interface to calendars using caldav.
+    Interface to calendar server using caldav.
     """
     def __init__(self, url):
+        self.url = url
         self.client = caldav.DAVClient(url)
         logging.debug('client: {}'.format(self.client))
         self.principal = self.client.principal()
@@ -17,18 +19,48 @@ class CalDAVserver():
         self.calendars = self.principal.calendars()
         logging.debug('calendars: {}'.format(self.calendars))
 
+        # get calendar IDs
+        props = [c.get_properties([caldav.elements.dav.Href(),
+                                   caldav.elements.dav.DisplayName()])
+                             for c in self.calendars]
+        self.calendar_IDs = [p['{DAV:}href'] for p in props]
+        self.calendar_DisplayNames = [p['{DAV:}displayname'] for p in props]
+            
+        
     def create_calendar(self, name, cal_id):
         """
         Create a new calendar on this server.
         name: Displayname of calendar
-        cal_id:
+        cal_id: unique ID
         """
         cal_id = sanity_check.trailing_slash(cal_id)
+        if any([i.endswith(cal_id) for i in self.calendar_IDs]):
+            raise Exception("ID {} already exists in url {}.".format(cal_id, self.url))
+        if name in self.calendar_DisplayNames:
+            raise Exception("Calendar name {} already exists in url {}.".format(name, self.url))
         try:
             self.principal.make_calendar(name=name, cal_id=cal_id)
             logging.info("Created new calendar {}, {}".format(name, cal_id))
             self.calendars = self.principal.calendars()
             logging.debug('calendars: {}'.format(self.calendars))
+            self.calendar_IDs.append(cal_id)
         except Exception as e:
             print(e)
             logging.info("Creation of new calendar {}, {} failed".format(name, cal_id))
+
+
+
+    def delete_calendar(self, cal_id):
+        """
+        Delete calendar with ID cal_id.
+        """
+        cal_id = sanity_check.trailing_slash(cal_id)
+        try:
+            for c in self.calendars:
+                if c.get_properties([caldav.elements.dav.Href()])['{DAV:}href'].endswith(cal_id):
+                    c.delete()
+                    logging.info("Deleted calendar with ID {}".format(cal_id))
+                    return
+            raise Exception("Calendar with ID {} does not exist.".format(cal_id))
+        except Exception as e:
+            print(e)
--- src/main.py
+++ src/main.py
@@ -41,7 +41,22 @@ def add(*args):
         cal_id = args['calendar'][2]
         if url_nick in servers:
             servers[url_nick].create_calendar(cal_name, cal_id)
-        
+        else:
+            raise Exception("URL nick not known")
+
+
+def delete(*args):
+    """
+    Handler to delete things.
+    """
+    args = vars(args[0])
+    if "calendar" in args:
+        url_nick = args['calendar'][0]
+        cal_id = args['calendar'][1]
+        if url_nick in servers:
+            servers[url_nick].delete_calendar(cal_id)
+        else:
+            raise Exception("URL nick not known")
 
         
 if __name__ == "__main__":
@@ -57,12 +72,18 @@ if __name__ == "__main__":
     parser_show.add_argument("--calendars", action='store_true', help="show calendars")
     parser_show.set_defaults(func=show)
 
-    parser_add = subparser.add_parser("add", help="Add calendars, events, etc.")
+    parser_add = subparser.add_parser("add", help="Add calendars, etc.")
     parser_add.add_argument("--calendar", nargs=3,
                                metavar=('URL_nick', 'calendar_name', 'cal_id'),
                                help="add a new calendar ")
     parser_add.set_defaults(func=add)
 
+    parser_del = subparser.add_parser("del", help="Delete calendars, etc.")
+    parser_del.add_argument("--calendar", nargs=2,
+                            metavar=("URL nick, cal_id"),
+                            help="Delete calendar from URL.")
+    parser_del.set_defaults(func=delete)
+    
     args = parser.parse_args()
 
 
@@ -74,6 +95,7 @@ if __name__ == "__main__":
 
     config.url = {k: sanity_check.trailing_slash(v) for k,v in config.url.items()}
 
+    
     for k, v in config.url.items():
         servers[k] = caldavserver.CalDAVserver(v)
         for k,v in servers.items():
