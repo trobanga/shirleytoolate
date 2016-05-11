f45f5fc133d4bf95834f1b5d23e377eb6b0badac
trobanga <d@niel.re>
2016-05-11 23:24:43 +0200

    new calendars representation

--- src/caldavserver.py
+++ src/caldavserver.py
@@ -26,22 +26,13 @@ class CalDAVserver():
         """
         Update list of dict of calendar, cal_id, and displayname
         """
-        calendars = self.principal.calendars()
-        
-        # get calendar IDs
-        props = [c.get_properties([caldav.elements.dav.Href(),
-                                   caldav.elements.dav.DisplayName()])
-                             for c in calendars]
-        cal_ids = [p['{DAV:}href'] for p in props]
+        calendars = self.principal.calendars()        
+        props = [c.get_properties([caldav.elements.dav.DisplayName()]) for c in calendars]
         cal_displaynames = [p['{DAV:}displayname'] for p in props]
-
-        self.calendars = [{"calendar": c,
-                           "cal_id": cal_id,
-                           "displayname": dn} for c, cal_id, dn in zip(calendars,
-                                                                       cal_ids,
-                                                                       cal_displaynames)]
+        self.calendars = dict(zip(cal_displaynames, calendars))
         logging.debug('CalDAVserver.__init__.calendars: {}'.format(self.calendars))
 
+        
                
     def create_calendar(self, displayname, cal_id):
         """
@@ -49,17 +40,22 @@ class CalDAVserver():
         name: Displayname of calendar
         cal_id: unique ID
         """
-        cal_id = sanity_check.trailing_slash(cal_id)
-        if any([c['cal_id'].endswith(cal_id) for c in self.calendars]):
-            raise Exception("CalDAVserver.create_calendar: ID {} already exists in url {}.".format(cal_id, self.url))
-        if any([c['displayname'] == displayname for c in self.calendars]):
+
+        if displayname in self.calendars:
             raise Exception("CalDAVserver.create_calendar: Calendar name {} already exists in url {}.".format(name, self.url))
+
+        # check if cal_id exists
+        cal_id = sanity_check.trailing_slash(cal_id)
+        calendars = self.principal.calendars()        
+        props = [c.get_properties([caldav.elements.dav.Href()]) for c in calendars]
+        cal_ids = [p['{DAV:}href'] for p in props]
+        if any([i.endswith(cal_id) for i in cal_ids]):
+            raise Exception("CalDAVserver.create_calendar: ID {} already exists in url {}.".format(cal_id, self.url))
+        
         try:
             new_calendar = self.principal.make_calendar(name=displayname, cal_id=cal_id)
             logging.info("Created new calendar {}, {}".format(displayname, cal_id))
-            self.calendars.append({'calendar': new_calendar,
-                                   'cal_id': cal_id,
-                                   'displayname': displayname})
+            self.calendars[displayname] = new_calendar
             logging.debug('CalDAVserver.create_calendar: current calendars: {}'.format(self.calendars))
             logging.info("Added new calendar {} with ID {} to url {}.".format(displayname, cal_id, self.url))
         except Exception as e:
@@ -71,13 +67,13 @@ class CalDAVserver():
         Delete calendar with ID cal_id.
         """
         try:
-            for c in self.calendars:
-                if c['displayname'] == displayname:
-                    c['calendar'].delete()
-                    logging.info("Deleted calendar with ID {} from url {}".format(c['cal_id'], self.url))
-                    self.update_calendars()
-                    return
-            raise Exception("CalDAVserver.delete_calendar: Calendar with ID {} does not exist in url.".format(c['cal_id'], self.url))
+            if displayname in self.calendars:
+                self.calendars[displayname].delete()
+                logging.info("Deleted calendar {} from url {}".format(displayname, self.url))
+                del(self.calendars[displayname])
+                return
+            else:
+                raise Exception("CalDAVserver.delete_calendar: Calendar {} does not exist in url.".format(displayname, self.url))
         except Exception as e:
             logging.debug("CalDAVserver.delete_calendar: Exception: {}".format(e))
 
@@ -86,21 +82,20 @@ class CalDAVserver():
         """
         Add event e to calendar with ID cal_id.
         """
-        for c in self.calendars:
-            if c['displayname'] == displayname:
-                try:
-                    c.add_event(e)
-                except Exception as e:
-                    logging.debug("CalDAVserver.add_event: Exception: {}".format(e))
+        if displayname in self.calendars:
+            try:
+                self.calendars[displayname].add_event(e)
+            except Exception as e:
+                logging.debug("CalDAVserver.add_event: Exception: {}".format(e))
             
 
     def get_events(self, displayname):
         """
         Returns a list of events of calendar displayname.
         """
-        for c in self.calendars:
-            if c['displayname'] == displayname:
-                return c.events()
-                
+        if displayname in self.calendars:
+            return self.calendars[displayname].events()
+        else:
+            return []
 
                 
